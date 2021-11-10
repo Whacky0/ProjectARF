524ea6108b3925d6c8c5a1ca1995ed402c528d93
EzequielCorrea1 <62712306+Whacky0@users.noreply.github.com>
2021-11-10 16:29:49 -0300

    ..

--- Assets/Prefab - c/PlaceOnPlane.cs
+++ Assets/Prefab - c/PlaceOnPlane.cs
@@ -19,7 +19,18 @@ namespace UnityEngine.XR.ARFoundation.Samples
         [SerializeField]
         [Tooltip("Instantiates this prefab on a plane at the touch location.")]
         GameObject m_PlacedPrefab;
-@ -33,9 +34,15 @@ namespace UnityEngine.XR.ARFoundation.Samples
+
+        /// <summary>
+        /// The prefab to instantiate on touch.
+        /// </summary>
+        public GameObject placedPrefab
+        {
+            get { return m_PlacedPrefab; }
+            set { m_PlacedPrefab = value; }
+        }
+
+        /// <summary>
+        /// The object instantiated as a result of a successful raycast intersection with a plane.
         /// </summary>
         public GameObject spawnedObject { get; private set; }
 
@@ -35,32 +46,46 @@ namespace UnityEngine.XR.ARFoundation.Samples
         }
 
         bool TryGetTouchPosition(out Vector2 touchPosition)
-@ -50,6 +57,7 @@ namespace UnityEngine.XR.ARFoundation.Samples
+        {
+            if (Input.touchCount > 0)
+            {
+                touchPosition = Input.GetTouch(0).position;
+                return true;
+            }
+
+            touchPosition = default;
             return false;
         }
 
-    [System.Obsolete]
-    void Update()
-    {
-        if (!TryGetTouchPosition(out Vector2 touchPosition))
-@ -64,16 + 72,20 @@
+        [System.Obsolete]
+        void Update()
+        {
+            if (!TryGetTouchPosition(out Vector2 touchPosition))
+                return;
+
+            if (m_RaycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
+            {
+                // Raycast hits are sorted by distance, so the first one
+                // will be the closest hit.
+                var hitPose = s_Hits[0].pose;
+
                 if (spawnedObject == null)
-        {
-            spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
-            planeManager.detectionMode = PlaneDetectionMode.None;
-            cloudManager.enabled = false;
+                {
+                    spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
+                    planeManager.detectionMode = PlaneDetectionMode.None;
+                    cloudManager.enabled = false;
+                }
+                else
+                {
+                    spawnedObject.transform.position = hitPose.position;
+                    planeManager.detectionMode = PlaneDetectionMode.Horizontal;
+                    cloudManager.enabled = true;
+                }
+            }
         }
-        else
-        {
-            spawnedObject.transform.position = hitPose.position;
-            planeManager.detectionMode = PlaneDetectionMode.Horizontal;
-            cloudManager.enabled = true;
-        }
-    }
-}
 
-static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+        static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
 
-ARRaycastManager m_RaycastManager;
+        ARRaycastManager m_RaycastManager;
     }
 }
