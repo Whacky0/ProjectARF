5da00e7ca8d309692b6d944718e91dcd744ef8e6
EzequielCorrea1 <88894972+EzequielCorrea1@users.noreply.github.com>
2021-11-10 13:58:04 -0300

    plane detection desactivate and points too
    Co-Authored-By: chicle3899 <70299240+chicle3899@users.noreply.github.com>

--- Assets/Prefab - c/PlaceOnPlane.cs
+++ Assets/Prefab - c/PlaceOnPlane.cs
@@ -15,6 +15,7 @@ namespace UnityEngine.XR.ARFoundation.Samples
     [RequireComponent(typeof(ARRaycastManager))]
     public class PlaceOnPlane : MonoBehaviour
     {
+        
         [SerializeField]
         [Tooltip("Instantiates this prefab on a plane at the touch location.")]
         GameObject m_PlacedPrefab;
@@ -33,9 +34,15 @@ namespace UnityEngine.XR.ARFoundation.Samples
         /// </summary>
         public GameObject spawnedObject { get; private set; }
 
+        ARPlaneManager planeManager;
+        ARPointCloudManager cloudManager;
+        
+        
         void Awake()
         {
             m_RaycastManager = GetComponent<ARRaycastManager>();
+            planeManager = this.GetComponent<ARPlaneManager>();
+            cloudManager= this.GetComponent<ARPointCloudManager>();
         }
 
         bool TryGetTouchPosition(out Vector2 touchPosition)
@@ -50,6 +57,7 @@ namespace UnityEngine.XR.ARFoundation.Samples
             return false;
         }
 
+        [System.Obsolete]
         void Update()
         {
             if (!TryGetTouchPosition(out Vector2 touchPosition))
@@ -64,10 +72,14 @@ namespace UnityEngine.XR.ARFoundation.Samples
                 if (spawnedObject == null)
                 {
                     spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
+                    planeManager.detectionMode = PlaneDetectionMode.None;
+                    cloudManager.enabled = false;
                 }
                 else
                 {
                     spawnedObject.transform.position = hitPose.position;
+                    planeManager.detectionMode = PlaneDetectionMode.Horizontal;
+                    cloudManager.enabled = true;
                 }
             }
         }
