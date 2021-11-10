ac37264f275d79e02de3a0d3293ec1a81e8a45c8
EzequielCorrea1 <62712306+Whacky0@users.noreply.github.com>
2021-11-10 15:09:15 -0300

    .
    Co-Authored-By: chicle3899 <70299240+chicle3899@users.noreply.github.com>

--- Assets/Prefab - c/PlaceOnPlane.cs
+++ Assets/Prefab - c/PlaceOnPlane.cs
@@ -15,65 +15,52 @@ namespace UnityEngine.XR.ARFoundation.Samples
     [RequireComponent(typeof(ARRaycastManager))]
     public class PlaceOnPlane : MonoBehaviour
     {
+
         [SerializeField]
         [Tooltip("Instantiates this prefab on a plane at the touch location.")]
         GameObject m_PlacedPrefab;
-
-        /// <summary>
-        /// The prefab to instantiate on touch.
-        /// </summary>
-        public GameObject placedPrefab
-        {
-            get { return m_PlacedPrefab; }
-            set { m_PlacedPrefab = value; }
-        }
-
-        /// <summary>
-        /// The object instantiated as a result of a successful raycast intersection with a plane.
+@ -33,9 +34,15 @@ namespace UnityEngine.XR.ARFoundation.Samples
         /// </summary>
         public GameObject spawnedObject { get; private set; }
+
+        ARPlaneManager planeManager;
+        ARPointCloudManager cloudManager;
+
 
         void Awake()
         {
             m_RaycastManager = GetComponent<ARRaycastManager>();
+            planeManager = this.GetComponent<ARPlaneManager>();
+            cloudManager = this.GetComponent<ARPointCloudManager>();
         }
 
         bool TryGetTouchPosition(out Vector2 touchPosition)
-        {
-            if (Input.touchCount > 0)
-            {
-                touchPosition = Input.GetTouch(0).position;
-                return true;
-            }
-
-            touchPosition = default;
+@ -50,6 +57,7 @@ namespace UnityEngine.XR.ARFoundation.Samples
             return false;
         }
 
-        void Update()
-        {
-            if (!TryGetTouchPosition(out Vector2 touchPosition))
-                return;
-
-            if (m_RaycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
-            {
-                // Raycast hits are sorted by distance, so the first one
-                // will be the closest hit.
-                var hitPose = s_Hits[0].pose;
-
+    [System.Obsolete]
+    void Update()
+    {
+        if (!TryGetTouchPosition(out Vector2 touchPosition))
+@ -64,16 + 72,20 @@
                 if (spawnedObject == null)
-                {
-                    spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
-                }
-                else
-                {
-                    spawnedObject.transform.position = hitPose.position;
-                }
-            }
+        {
+            spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
+            planeManager.detectionMode = PlaneDetectionMode.None;
+            cloudManager.enabled = false;
         }
+        else
+        {
+            spawnedObject.transform.position = hitPose.position;
+            planeManager.detectionMode = PlaneDetectionMode.Horizontal;
+            cloudManager.enabled = true;
+        }
+    }
+}
 
-        static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
 
-        ARRaycastManager m_RaycastManager;
+ARRaycastManager m_RaycastManager;
     }
 }
